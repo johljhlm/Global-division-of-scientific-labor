62b03c2a6aa0ce14291a3edbd37c2385f270b798
france122 <france6259@163.com>
2025-06-05 15:28:32 +0800

    refresh things and add new functions

--- js/descriptive.js
+++ js/descriptive.js
@@ -26,10 +26,10 @@ let currentChart = null;
 document.addEventListener('DOMContentLoaded', function () {
     // 加载所有数据
     Promise.all([
-        d3.csv('../../data/data_gdp_clean_long.csv'),
-        d3.csv('../../data/GDP_PPP_CI.csv'),
-        d3.csv('../../data/ECI_Ranking.csv'),
-        d3.csv('../../data/income_group.csv')
+        d3.csv('../data/data_gdp_clean_long.csv'),
+        d3.csv('../data/GDP_PPP_CI.csv'),
+        d3.csv('../data/ECI_Ranking.csv'),
+        d3.csv('../data/income_group.csv')
     ]).then(([gdp, ppp, eci, income]) => {
         gdpData = gdp;
         pppData = ppp;
